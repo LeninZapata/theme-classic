148cafdafa6788a58d204dbbe48ebb3d0c8085f4
LeninZapata <ilen.elfuerte@gmail.com>
2020-04-02 20:38:07 -0500

    Init 0.1

---
+++ assets/js/js.js
@@ -0,0 +1,16 @@
+jQuery(document).ready(function( $ ){
+	var $ = jQuery;
+	var btn = $('.theme-button-back-to-top');
+	$(window).scroll(function() {
+		if ($(window).scrollTop() > 300) {
+			btn.addClass('show');
+		} else {
+			btn.removeClass('show');
+		}
+	});
+
+	btn.on('click', function(e) {
+		e.preventDefault();
+		$('html, body').animate({scrollTop:0}, '300');
+	});
+})
